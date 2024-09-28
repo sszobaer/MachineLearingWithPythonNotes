c3517d2ec48546aee6c9a071144709fdb26707cc
S. S. Zobaer Ahmed <118447914+sszobaer@users.noreply.github.com>
2024-09-28 19:13:16 +0600

    Add files via upload

---
+++ Codes/Basic Codes/Operators/Operators.py
@@ -0,0 +1,13 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+#Exponential
+c = a**b #power a^b
+print(c)
+
+#Assignment Operator
+a*=6 # a= a-5  a= a-6 a= 10*6
+a+=5
+
+#Logical Operator
+print(not((a>=b)|(a>b)|(a==b)|(a==b)))
